b0d54ee3e65840d964146fe7e8a4e82fea585d95
Nicolas Schmid <nicolas.schmid@mobi.ch>
2024-10-05 21:09:16 +0200

    deploy page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
@@ -6,10 +6,12 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mydatastack.tech',
-  integrations: [react(), tailwind(), robotsTxt(
-      {
-        sitemap: 'https://mydatastack.tech/sitemap.xml'
-      }
-  )]
+    site: 'https://mydatastack.tech',
+    output: 'static',
+    outDir: 'docs',
+    integrations: [react(), tailwind(), robotsTxt(
+        {
+            sitemap: 'https://mydatastack.tech/sitemap.xml'
+        }
+    )]
 });
