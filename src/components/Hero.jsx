264b03842de1c2e512e8dd84897880ab898e1432
Nicolas Schmid <nicolas.schmid@mobi.ch>
2024-10-05 21:46:54 +0200

    add pipeline

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
           transition={{ duration: 0.5 }}
         >
           <div className="text-customSecondary text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
-            Discover MyDATA
+            Discover MyDATAStack.tech
           </div>
         </motion.div>
         <motion.div
